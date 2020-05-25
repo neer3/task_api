21f6b9bfc0a4f52b9d66d11ae270deb2c3d15494
Neeraj Singh <41228889+ne30@users.noreply.github.com>
2020-05-25 22:12:55 +0530

    id removal

--- task/settings.py
+++ task/settings.py
@@ -20,7 +20,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 # See https://docs.djangoproject.com/en/2.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = '0gst*)io!w4a1z$hc)84t@wib-0d*m=hm3d--1p@q68l=*$uva'
+SECRET_KEY = ' '
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
